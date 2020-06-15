8902abadf20c2ba8ebf951a9b634798b7a5e9d91
dv.kazancev <kazahec@mail.ru>
2020-06-15 15:41:48 +0300

    initial app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Modal, Button, Jumbotron, FormControl} from "react-bootstrap";
 
 function App() {
+  const [name, setName] = useState('anonym');
+
+  const [show, setOpened] = useState(false);
+  const handleClose = () => setOpened(false);
+  const handleOpen = () => setOpened(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron>
+        <h1>
+          Hello, <span>{name}</span>!
+        </h1>
+        <Button variant="primary" onClick={handleOpen}>Enter name</Button>
+      </Jumbotron>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Please, enter your name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormControl
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Dmitry K."
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Ok!
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
